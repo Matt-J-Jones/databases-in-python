dbdb1aa1fe1ae91bcacebd817c5375a3243d1b01
Matt-J-Jones <aratinlondon@gmail.com>
2024-01-24 13:52:26 +0000

    test driven book class

--- 02_Phase_2/02_book_store/tests/test_book.py
+++ 02_Phase_2/02_book_store/tests/test_book.py
@@ -1,2 +1,16 @@
 from lib.book import Book
 
+def test_book_constructs():
+    book = Book(1, "Test Title", "Test Author")
+    assert book.id == 1
+    assert book.title == "Test Title"
+    assert book.author_name == "Test Author"
+
+def test_books_format_nicely():
+    book = Book(1, "Test Title", "Test Author")
+    assert str(book) == "1 - Test Title - Test Author"
+
+def test_books_are_equal():
+    book_1 = Book(1, "Test Title", "Test Author")
+    book_2 = Book(1, "Test Title", "Test Author")
+    assert book_1 == book_2
